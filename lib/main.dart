eeb3bf6c56e3405e4a66013b4052ded8fbd3e695
Thulasi <thulasicsr@gmail.com>
2021-05-14 13:32:10 +0530

    this quiz project

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "QuizStar",
+      title: "QuizStar ",
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
